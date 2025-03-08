34959d407d3e27d11bd5c8d4d59014781f45f086
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-03-08 20:39:34 +0100

    add the gateway project

---
+++ gateway/src/main/java/com/jonathanfoucher/gatewayexample/GatewayExampleApplication.java
@@ -0,0 +1,11 @@
+package com.jonathanfoucher.gatewayexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayExampleApplication.class, args);
+    }
+}
